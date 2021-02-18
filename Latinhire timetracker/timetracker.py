7257b89104496358fb8c0dba904da0be929636d9
csanchez <csanchez@grupodot.com>
2021-02-17 20:26:14 -0500

    Added dropdown menu and texts labels that will contain the text for each case in the dropdown menu

--- Latinhire timetracker/timetracker.py
+++ Latinhire timetracker/timetracker.py
@@ -2,13 +2,18 @@ from tkinter import *
 from datetime import datetime 
 counter = 66600
 running = False 
-
+text1= 'HOLA AMIGUITO'
+text2='TE AMO CARRITO'
 def f1(label): 
     label['text'] = 'Start'
 def f2(label): 
     label['text'] = 'Reset'
 def f3(label): 
     label['text'] = 'Stop'
+def copy_text_to_clipboard(event): 
+    text = event.widget.get("1.0","end-1c")
+    window.clipboard_clear()
+    window.clipboard_append(text)
 
 tt = datetime.fromtimestamp(counter)
 string = tt.strftime("%H:%M:%S")
@@ -41,12 +46,38 @@ total_working_time = Label(window, text=string, fg='black', font='Verdana 20')
 total_working_time.place(x=550,y=200)
 
 language_var = IntVar(window,0)
-Label(window, text='Language:', fg='black', font='Verdana 10 bold').place(x=325,y=250)
-Radiobutton(window, text='Spanish', value=0, variable=language_var).place(x=400,y=250)
-Radiobutton(window, text='English', value=1, variable=language_var).place(x=500,y=250)
+Label(window, text='Language:', fg='black', font='Verdana 10 bold').place(x=375,y=250)
+Radiobutton(window, text='Spanish', value=0, variable=language_var).place(x=460,y=250)
+Radiobutton(window, text='English', value=1, variable=language_var).place(x=560,y=250)
 #for i in range(1,4): 
 #    Radiobutton(window, text='holi %i' %i, value=i, variable=var).pack()
 #Button(window,text='holitas', command = lambda: print(var.get())).pack()
+Label(window, text='Case:', fg='black', font='Verdana 10 bold').place(x=0,y=250)
+cases = ['a','b','c']
+case_select_var = StringVar(window)
+case_select_var.set(cases[0])
+
+options = OptionMenu(window,case_select_var,*cases)
+options.config(width=28, font=('Verdana',12))
+options.place(x=50,y=250)
+
+texts_frame = Frame(window)
+texts_frame.place(x=0,y=300)
+text1_title = Label(texts_frame, text='Text 1', font='Verdana 12 bold')
+text1_title.pack()
+text_field1 = Text(texts_frame,height=4,borderwidth=0)
+text_field1.insert(1.0,text1)
+text_field1.pack()
+text_field1.bind("<Button-1>",copy_text_to_clipboard)
+
+text2_title = Label(texts_frame, text='Text 2', font='Verdana 12 bold')
+text2_title.pack()
+text_field2 = Text(texts_frame,height=4,borderwidth=0)
+text_field2.insert(1.0,text2)
+text_field2.pack()
+
+text_field2.bind("<Button-1>",copy_text_to_clipboard)
+
 window.mainloop()
 
 
