cd807263c35c5ab81ef26c21653d5a9836b54189
csanchez <csanchez@grupodot.com>
2021-02-27 00:50:28 -0500

    Modified DataWriter.py structure and redefined it as a class

--- Latinhire timetracker/DataWriter.py
+++ Latinhire timetracker/DataWriter.py
@@ -1,10 +1,35 @@
 from pydrive.auth import GoogleAuth
 from pydrive.drive import GoogleDrive
-import os 
-print(os.getcwd())
-gauth = GoogleAuth()
-gauth.LocalWebserverAuth()
-drive = GoogleDrive(gauth)
-fileList = drive.ListFile({'q': "'root' in parents and trashed=false"}).GetList()
-for file in fileList: 
-    print('Title: %s, ID: %s' % (file['title'], file['id']))
+
+class DataWriter: 
+    def __init__(self): 
+        self.gauth = GoogleAuth()
+        self.gauth.LocalWebserverAuth()
+        self.drive = GoogleDrive(self.gauth)
+    def createSheet(self,SheetTitle,FolderId=None):
+        '''Function that creates a google sheets at the specified folder. If no folder 
+        is specified, the sheet is created at the root
+        Inputs: 
+        -SheetTitle: The title of the sheet to be created 
+        -FolderId: The ID of the folder where the sheet will be created 
+        Outputs: 
+        -SheetId: The ID of the created sheet'''
+        if FolderId ==None: 
+            file = self.drive.CreateFile({'title':SheetTitle,'mimeType':'application/vnd.google-apps.spreadsheet'})
+        else:            
+            file = self.drive.CreateFile({'title':SheetTitle,'mimeType':'application/vnd.google-apps.spreadsheet',
+            'parents': [{'kind': 'drive#fileLink','id': FolderId}]})
+        file.Upload()
+        SheetId=file['id']
+        return SheetId
+
+    def createFolder(self,FolderTitle):
+        '''Function that creates a folder with title FolderTitle. The folder is created at the root.
+        Inputs: 
+        -FolderTitle: The title of the folder to be created 
+        Outputs: 
+        -FolderId: The ID of the created folder'''
+        folder = self.drive.CreateFile({'title':FolderTitle,'mimeType':'application/vnd.google-apps.folder'})
+        folder.Upload()
+        FolderId = folder['id']
+        return FolderId
