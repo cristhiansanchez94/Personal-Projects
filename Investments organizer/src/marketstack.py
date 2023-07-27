6b102fb322f8b3a94ed5399ab1ec045d7bbcd319
Cristhian Sanchez <csanchez@smarthop.co>
2023-07-27 13:53:14 +0200

    Include function to fetch current prices of a stock

---
+++ Investments organizer/src/marketstack.py
@@ -0,0 +1,19 @@
+import requests 
+import json 
+import os 
+
+
+MS_API_KEY = os.environ.get('ms_api_key')
+BASE_URL = "http://api.marketstack.com/v1/"
+
+def get_stock_price(stock_symbols):
+    '''Function used to fetch the current price of a stock ticker'''
+    params={
+        'access_key': MS_API_KEY
+    }
+    end_point = ''.join([BASE_URL,"tickers/",stock_symbols,"/intraday/latest"])
+    api_result = requests.get(end_point, params)
+    json_result = json.loads(api_result.text)
+    return {
+        "last_price": json_result['last']
+    }
