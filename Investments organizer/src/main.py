7c56ba8ad7a617bc1865b08c9acfa88c76854e45
Cristhian Sanchez <csanchez@smarthop.co>
2023-07-27 15:14:38 +0200

    Implemented new labels to include the current price gathered with the API

--- Investments organizer/src/main.py
+++ Investments organizer/src/main.py
@@ -28,7 +28,7 @@ def get_info_for_ticker(ticker):
 def set_values_in_vars(ticker_info): 
     avg_price_var.set(ticker_info.get('average_price'))
     real_current_price_var.set(ticker_info.get('last_price'))
-    current_value_var.set(ticker_info.get('current_value'))
+    current_value_var.set(round(ticker_info.get('current_value'),2))
     fifo_avg_price_var.set(str(ticker_info.get('fifo_average_price'))+"/"+str(ticker_info.get('fifo_average_price_normal')))
     first_movement_var.set(datetime.strftime(ticker_info.get('min_date'),'%Y-%m-%d'))
     last_movement_var.set(datetime.strftime(ticker_info.get('max_date'),'%Y-%m-%d'))
@@ -109,7 +109,7 @@ def calculate_new_average_price(*args):
 
 window = Tk()
 window.title('Stock controller')
-window.geometry("600x550")
+window.geometry("600x600")
 window.resizable(0,0)
 
 #Variables 
@@ -142,82 +142,92 @@ total_investment.place(x=LABELS_X, y=20)
 share_label = Label(window, text='Share:', fg='black', font='Verdana 18')
 share_label.place(x=0,y=50)
 
+real_current_price_title = Label(window, text='Current price:', fg='black', font='Verdana 18')
+real_current_price_title.place(x=0,y=80)
+real_current_price_label = Label(window, textvariable=real_current_price_var, fg='black', font='Verdana 16')
+real_current_price_label.place(x=LABELS_X, y=80)
+
+current_value_title = Label(window, text='Current value:', fg='black', font='Verdana 18')
+current_value_title.place(x=0,y=110)
+current_value_label = Label(window, textvariable=current_value_var, fg='black', font='Verdana 16')
+current_value_label.place(x=LABELS_X, y=110)
+
 avg_price_title = Label(window, text='Avg. price:', fg='black', font='Verdana 18')
-avg_price_title.place(x=0,y=80)
+avg_price_title.place(x=0,y=140)
 avg_price_label = Label(window, textvariable=avg_price_var, fg='black', font='Verdana 16')
-avg_price_label.place(x=LABELS_X, y=80)
+avg_price_label.place(x=LABELS_X, y=140)
 
 fifo_avg_price = Label(window, text='Avg. price (FIFO):', fg='black', font='Verdana 18')
-fifo_avg_price.place(x=0,y=110)
+fifo_avg_price.place(x=0,y=170)
 fifo_avg_price_label = Label(window, textvariable=fifo_avg_price_var, fg='black', font='Verdana 16')
-fifo_avg_price_label.place(x=LABELS_X, y=110)
+fifo_avg_price_label.place(x=LABELS_X, y=170)
 
 first_movement_title = Label(window, text='First movement:', fg='black', font='Verdana 18')
-first_movement_title.place(x=0,y=140)
+first_movement_title.place(x=0,y=200)
 first_movement_label = Label(window, textvariable=first_movement_var, fg='black', font='Verdana 16')
-first_movement_label.place(x=LABELS_X, y=140)
+first_movement_label.place(x=LABELS_X, y=200)
 
 last_movement_title = Label(window, text='Last movement:', fg='black', font='Verdana 18')
-last_movement_title.place(x=0,y=170)
+last_movement_title.place(x=0,y=230)
 last_movement_label = Label(window, textvariable=last_movement_var, fg='black', font='Verdana 16')
-last_movement_label.place(x=LABELS_X, y=170)
+last_movement_label.place(x=LABELS_X, y=230)
 
 available_stocks_title = Label(window, text='Available stocks:', fg='black', font='Verdana 18')
-available_stocks_title.place(x=0,y=200)
+available_stocks_title.place(x=0,y=260)
 available_stocks_label = Label(window, textvariable=available_stocks_var, fg='black', font='Verdana 16')
-available_stocks_label.place(x=LABELS_X, y=200)
+available_stocks_label.place(x=LABELS_X, y=260)
 
 remaining_stocks_title = Label(window, text='Remaining stocks:', fg='black', font='Verdana 18')
-remaining_stocks_title.place(x=0,y=230)
+remaining_stocks_title.place(x=0,y=290)
 remaining_stocks_label = Label(window, textvariable=remaining_stocks_var, fg='black', font='Verdana 16')
-remaining_stocks_label.place(x=LABELS_X, y=230)
+remaining_stocks_label.place(x=LABELS_X, y=290)
 
 total_invested_amount_title = Label(window, text='Total invested amount (stock):', fg='black', font='Verdana 18')
-total_invested_amount_title.place(x=0,y=260)
+total_invested_amount_title.place(x=0,y=320)
 total_invested_amount_label = Label(window, textvariable=total_invested_amount_var, fg='black', font='Verdana 16')
-total_invested_amount_label.place(x=LABELS_X, y=260)
+total_invested_amount_label.place(x=LABELS_X, y=320)
 
 total_sold_amount_title= Label(window, text='Total sold amount:', fg='black', font='Verdana 18')
-total_sold_amount_title.place(x=0,y=290)
+total_sold_amount_title.place(x=0,y=350)
 total_sold_amount_label = Label(window, textvariable=total_sold_amount_var, fg='black', font='Verdana 16')
-total_sold_amount_label.place(x=LABELS_X, y=290)
+total_sold_amount_label.place(x=LABELS_X, y=350)
 
 portfolio_percentage_title= Label(window, text='Portfolio percentage:', fg='black', font='Verdana 18')
-portfolio_percentage_title.place(x=0,y=320)
+portfolio_percentage_title.place(x=0,y=380)
 portfolio_percentage_label = Label(window, textvariable=portfolio_percentage_var, fg='black', font='Verdana 16')
-portfolio_percentage_label.place(x=LABELS_X, y=320)
+portfolio_percentage_label.place(x=LABELS_X, y=380)
 
 
 
 #Avg price calculator 
 
 avg_price_calculator_title = Label(window, text='Avg. price calculator', fg='black', font='Verdana 20 bold')
-avg_price_calculator_title.place(x=150,y=350)
+avg_price_calculator_title.place(x=150,y=410)
 
 current_price_title = WrappingLabel(window, text='Current price', fg='black', font='Verdana 18', width=10)
-current_price_title.place(x=0, y=390)
+current_price_title.place(x=0, y=450)
 current_price_entry = Entry(window, width=8, textvariable=current_price_var, justify='center', font=('verdana', 18))
-current_price_entry.place(x=15, y=440)
+current_price_entry.place(x=15, y=500)
 
 desired_avg_price_title = WrappingLabel(window, text='Desired avg. price', fg='black', font='Verdana 18', width=10)
-desired_avg_price_title.place(x=120, y=390)
+desired_avg_price_title.place(x=120, y=450)
 desired_avg_entry = Entry(window, width=8, textvariable=desired_avg_price_var, justify='center', font=('verdana', 18))
-desired_avg_entry.place(x=135, y=440)
+desired_avg_entry.place(x=135, y=500)
 
 required_amount_title = WrappingLabel(window, text='Required amount', fg='black', font='Verdana 18', width=10)
-required_amount_title.place(x=240, y=390)
+required_amount_title.place(x=240, y=450)
 required_amount_label = Label(window, textvariable=required_amount_var, fg='black', font='Verdana 18', width=10)
-required_amount_label.place(x=255, y=440)
+required_amount_label.place(x=255, y=500)
 
 amount_to_invest_title = WrappingLabel(window, text='Amount to invest', fg='black', font='Verdana 18', width=10)
-amount_to_invest_title.place(x=360, y=390)
+amount_to_invest_title.place(x=360, y=450)
 amount_to_invest_entry = Entry(window, width=8, textvariable=amount_to_invest_var, justify='center', font=('verdana', 18))
-amount_to_invest_entry.place(x=375, y=440)
+amount_to_invest_entry.place(x=375, y=500)
 
 new_avg_price_title = WrappingLabel(window, text='New avg. price', fg='black', font='Verdana 18', width=10)
-new_avg_price_title.place(x=480, y=390)
+new_avg_price_title.place(x=480, y=450)
 new_avg_price_label = Label(window, textvariable=new_avg_price_var, fg='black', font='Verdana 18', width=10)
-new_avg_price_label.place(x=495, y=440)
+new_avg_price_label.place(x=495, y=500)
 
 
 amount_to_invest_var.trace("w",calculate_new_average_price)
@@ -231,7 +241,7 @@ generate_report_button.pack(side='bottom',pady=30)
  
  #Stock selector
 case_select_var = StringVar(window)
-#case_select_var.set(TICKERS[0])
+case_select_var.set(TICKERS[0])
  
 options = OptionMenu(window, case_select_var, *TICKERS, command = update_vars) 
 options.config(width=15, font=('Verdana', 12))
